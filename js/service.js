371e8fe04dcc4522a6b505f84ce49a6a8969d63d
Sergio Lucas <sergioluk@gmail.com>
2016-10-24 14:20:48 +0200

    navSearch added in service

--- js/service.js
+++ js/service.js
@@ -4,4 +4,7 @@ angular.module('myApp')
 			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2")
 		
 		}
+		function getNavSearch(query){
+			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2&q=" + query )
+		}
 	})
