a1cec9fc80e94b4500a995d14ec0ae5f2c41905e
Oscar Leon <oscar.l.p.7@gmail.com>
2016-10-24 12:36:19 +0200

    Project folders

---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module('myApp', ['ngRoute'])
+	.config( function($routeProvider){
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/home.html',
+				controller: 'homeController'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'aboutController'
+			})
+			.when('/contact', {
+				templateUrl: 'views/contact.html',
+				controller: 'contactController'
+			})
+			.when('/search', {
+				templateUrl: 'views/search.html',
+				controller: 'searchController'
+			})
+	})
