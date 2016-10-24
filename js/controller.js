e63e3a90bc4145d2700b636de00634a5a5a60e23
Oscar Leon <oscar.l.p.7@gmail.com>
2016-10-24 15:08:45 +0200

    New version

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,24 @@
-angular.module('myApp')
+angular.module('myApp',)
 	.controller('homeController', function($scope){
 
 	})
-	.controller('aboutController', function($scope){
+	// .controller('aboutController', function($scope){
 
-	})
-	.controller('contactController', function($scope){
+	// })
+	// .controller('contactController', function($scope){
 		
-	})
-	.controller('contactController', function($scope){
+	// })
+	// .controller('contactController', function($scope){
 		
-	})
-	.controller('searchController', function($scope){
+	// })
+	// .controller('searchController', function($scope){
 		
-	})
-	.controller('randomController', function($scope){
-		$scope.getRandom=function(){
-			foodService.getRandom()
-				.then(function(response){
-					console.log(response);
-				})
-		}
-	})
+	// })
+	// .controller('randomController', function($scope){
+	// 	$scope.getRandom=function(){
+	// 		foodService.getRandom()
+	// 			.then(function(response){
+	// 				console.log(response);
+	// 			})
+	// 	}
+	// })
--- js/service.js
+++ js/service.js
@@ -1,12 +1,12 @@
 angular.module('myApp')
 	.factory('foodService', function($http){
 		function getRandom(){
-			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2")
+			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2");
+		}
+		function getNavSearch(query){
+			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2&q=" + query );
 		}
 		return {
 			getRandom: getRandom
-		}
-		function getNavSearch(query){
-			return $http.get("http://food2fork.com/api/search?key=a6b99a4152486ca85ac5a86da8d607a2&q=" + query )
 		}
 	})
