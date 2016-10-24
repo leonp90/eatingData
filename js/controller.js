33eccb2eb25d05108f1317d9960cc0a9f518f821
Oscar Leon <oscar.l.p.7@gmail.com>
2016-10-24 14:40:10 +0200

    Controllers and View Folders

---
+++ js/controller.js
@@ -0,0 +1,24 @@
+angular.module('myApp')
+	.controller('homeController', function($scope){
+
+	})
+	.controller('aboutController', function($scope){
+
+	})
+	.controller('contactController', function($scope){
+		
+	})
+	.controller('contactController', function($scope){
+		
+	})
+	.controller('searchController', function($scope){
+		
+	})
+	.controller('randomController', function($scope){
+		$scope.getRandom=function(){
+			foodService.getRandom()
+				.then(function(response){
+					console.log(response);
+				})
+		}
+	})
