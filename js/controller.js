f01cd8494a5f3099ddaa164c23f82ed89cb54ecf
Oscar Leon <oscar.l.p.7@gmail.com>
2016-10-25 10:54:53 +0200

    Controller update

--- js/controller.js
+++ js/controller.js
@@ -1,30 +1,62 @@
 angular.module('myApp')
-	.controller('homeController', function($scope, foodService){
-			$scope.getRandom=function(){
-				var random = Math.floor(Math.random() * (30 - 1) + 1);
-				foodService.getRandom()
-					.then(function(response){
-						$scope.recipeTitle = response.data[random].title
-						$scope.recipeImg = response.data[random].image_url
-						console.log(random);
-						console.log(response.data);
-				})
-		}
-		$scope.getRandom();
+.controller('homeController', function($scope, foodService){
+        var random = Math.floor(Math.random() * (30 - 1) + 1);
+            $scope.getRandom=function(){
+                //debugger;
+                foodService.getRandom()
+                    .then(function(response){
+                        $scope.recipeTitle = response.data[random].title;
+                        $scope.recipeImg = response.data[random].image_url;
+                        console.log(random);
+                        console.log(response.data);
+                        
+                });
+        };
+        $scope.getRandom();
+        $scope.getID = function(){
+				foodService.getDescription($scope.recipeID)
+				.then(function(response){
+					console.log(response);
+				});
+		};
 })
+// 	var random = Math.floor(Math.random() * (30 - 1) + 1);
+// 	$scope.getRandom=function(){
 
-	.controller('searchController', function($scope,$rootScope,foodService){
-		$scope.getRecipe=function(e){
-			e.preventDefault();
-			foodService.getNavSearch($scope.foodQuery)
-			.then(function (response){
+// 		foodService.getRandom()
+// 		.then(function(response){
+// 			$scope.recipeTitle = response.data[random].title
+// 			$scope.recipeImg = response.data[random].image_url
+// 			$scope.recipeID = response.data[random].url_details
+// 			console.log($scope.recipeID);
+// 			console.log(random);
+// 			console.log(response.data);
+// 			$scope.getID = function(){
+// 				foodService.getDescription($scope.recipeID)
+// 				.then(function(response){
+// 					console.log(response);
+// 				});
+// 			};
+// 		})
+
+// }
+// 		$scope.getRandom();
+// 		$scope.getID();
+// 	}
+
+		.controller('searchController', function($scope,$rootScope,foodService){
+			$scope.getRecipe=function(e){
+				e.preventDefault();
+				foodService.getNavSearch($scope.foodQuery)
+				.then(function (response){
 				// console.log(response.data);
 
 			})
 
-		}
+			}
 
-	})
+		})
+
 	// .controller('aboutController', function($scope){
 
 	// })
