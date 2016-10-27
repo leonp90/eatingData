b0fcd1492f7b689bfa8840d8d9829648e4eb3e84
ruben sanchez <rubenscejudo@yahoo.com>
2016-10-27 14:40:25 +0200

    Update controllers

--- js/controller.js
+++ js/controller.js
@@ -8,6 +8,7 @@ angular.module('myApp')
         $scope.recipeImg = response.data[random].image_url;
         recipeID += response.data[random].url_details;
         $scope.getID();
+   
     });
     $scope.getID = function() { 
         foodService.getDescription(recipeID)
@@ -20,9 +21,9 @@ angular.module('myApp')
         e.preventDefault();
         foodService.getNavSearch($scope.foodQuery)
         .then(function (response){
-            // console.log(response);
+                // console.log(response);
             $rootScope.recipes=response.data;
-            // console.log(response);
+                // console.log(response);
         })
         $location.path("/search")
     }
